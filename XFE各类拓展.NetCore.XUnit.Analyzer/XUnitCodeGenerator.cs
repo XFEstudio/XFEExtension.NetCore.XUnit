5594e7acea16062b831f327e5f3fba66fad658d8
XFEstudio <mail@xfegzs.com>
2024-02-11 06:38:35 +0800

    添加项目文件。

---
+++ XFE各类拓展.NetCore.XUnit.Analyzer/XUnitCodeGenerator.cs
@@ -0,0 +1,37 @@
+﻿using Microsoft.CodeAnalysis;
+
+namespace XFE各类拓展.NetCore.XUnit
+{
+    [Generator]
+    public class XUnitCodeGenerator : ISourceGenerator
+    {
+        public void Initialize(GeneratorInitializationContext context)
+        {
+        }
+
+        public void Execute(GeneratorExecutionContext context)
+        {
+            // find the main method
+            var mainMethod = context.Compilation.GetEntryPoint(context.CancellationToken);
+
+            // build up the source code
+            string source = $@"
+using XFE各类拓展.NetCore.XUnit;
+
+namespace {mainMethod.ContainingNamespace.Name}
+{{
+    public static class XUnitCode
+    {{
+        public async static Task Main(string[] args)
+        {{
+            await XFECode.RunTest();
+        }}
+    }}
+}}
+";
+            //var typeName = mainMethod.ContainingType.Name;
+            // add the source code to the compilation
+            context.AddSource($"XUnitCode.g.cs", source);
+        }
+    }
+}
