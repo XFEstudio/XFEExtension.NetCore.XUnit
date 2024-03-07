2193df651380d591c3880dddc1e8837f29581f32
XFEstudio <mail@xfegzs.com>
2024-03-07 22:51:03 +0800

    现在可以完美诊断错误信息并提供代码修补操作

---
+++ XFE各类拓展.NetCore.XUnit.Analyzer/CodeFix/XUnitCodeFixProvider.cs
@@ -0,0 +1,59 @@
+﻿using Microsoft.CodeAnalysis;
+using Microsoft.CodeAnalysis.CodeActions;
+using Microsoft.CodeAnalysis.CodeFixes;
+using Microsoft.CodeAnalysis.CSharp;
+using Microsoft.CodeAnalysis.CSharp.Syntax;
+using System.Collections.Immutable;
+using System.Linq;
+using System.Threading;
+using System.Threading.Tasks;
+using XFE各类拓展.NetCore.XUnit.Analyzer.Diagnostics;
+
+namespace XFE各类拓展.NetCore.XUnit.Analyzer.CodeFix
+{
+    [ExportCodeFixProvider(LanguageNames.CSharp, Name = nameof(XUnitCodeFixProvider))]
+    public class XUnitCodeFixProvider : CodeFixProvider
+    {
+        public override ImmutableArray<string> FixableDiagnosticIds => ImmutableArray.Create(XUnitCodeAnalyzer.SMTestID);
+
+        public override FixAllProvider GetFixAllProvider() => WellKnownFixAllProviders.BatchFixer;
+
+        public override Task RegisterCodeFixesAsync(CodeFixContext context)
+        {
+            foreach (var diagnostic in context.Diagnostics)
+            {
+                if (diagnostic.Id == XUnitCodeAnalyzer.SMTestID)
+                {
+                    context.RegisterCodeFix(CodeAction.Create(title: "改用MTest特性",
+                                                              createChangedDocument: c => ChangeSMTestToMTestAsync(context.Document, diagnostic, c),
+                                                              equivalenceKey: "改用MTest特性"),
+                                                              diagnostic: diagnostic);
+                    context.RegisterCodeFix(CodeAction.Create(title: "将方法改为静态方法",
+                                                              createChangedDocument: c => MakeMethodStaticAsync(context.Document, diagnostic, c),
+                                                              equivalenceKey: "将方法改为静态方法"),
+                                                              diagnostic: diagnostic);
+                }
+            }
+            return Task.CompletedTask;
+        }
+
+        public static async Task<Document> MakeMethodStaticAsync(Document document, Diagnostic diagnostic, CancellationToken cancellationToken)
+        {
+            var root = await document.GetSyntaxRootAsync(cancellationToken);
+            var attribute = root.FindToken(diagnostic.Location.SourceSpan.Start).Parent.AncestorsAndSelf().OfType<AttributeSyntax>().First();
+            var method = attribute.Parent.Parent as MethodDeclarationSyntax;
+            var newMethod = method.AddModifiers(SyntaxFactory.Token(SyntaxKind.StaticKeyword));
+            var newRoot = root.ReplaceNode(method, newMethod);
+            return document.WithSyntaxRoot(newRoot);
+        }
+
+        public static async Task<Document> ChangeSMTestToMTestAsync(Document document, Diagnostic diagnostic, CancellationToken cancellationToken)
+        {
+            var root = await document.GetSyntaxRootAsync(cancellationToken);
+            var attribute = root.FindToken(diagnostic.Location.SourceSpan.Start).Parent.AncestorsAndSelf().OfType<AttributeSyntax>().First();
+            var newAttribute = SyntaxFactory.Attribute(SyntaxFactory.ParseName("MTest"));
+            var newRoot = root.ReplaceNode(attribute, newAttribute);
+            return document.WithSyntaxRoot(newRoot);
+        }
+    }
+}
--- XFE各类拓展.NetCore.XUnit.Analyzer/Diagnostics/XUnitCodeAnalyzer.cs
+++ XFE各类拓展.NetCore.XUnit.Analyzer/Diagnostics/XUnitCodeAnalyzer.cs
@@ -1,30 +1,35 @@
 ﻿using Microsoft.CodeAnalysis;
 using Microsoft.CodeAnalysis.CSharp;
 using Microsoft.CodeAnalysis.CSharp.Syntax;
+using Microsoft.CodeAnalysis.Diagnostics;
+using System.Collections.Immutable;
 using System.Linq;
 
-namespace XFE各类拓展.NetCore.XUnit.Analyzer
+namespace XFE各类拓展.NetCore.XUnit.Analyzer.Diagnostics
 {
-    [Generator]
-    public class XUnitCodeAnalyzer : ISourceGenerator
+    [DiagnosticAnalyzer(LanguageNames.CSharp)]
+    public class XUnitCodeAnalyzer : DiagnosticAnalyzer
     {
-        public static readonly DiagnosticDescriptor SMTestError = new DiagnosticDescriptor("XFE0001",
+        public const string SMTestID = "XFE0001";
+        public static readonly DiagnosticDescriptor SMTestError = new DiagnosticDescriptor(SMTestID,
                                                                                            "不能标记非静态方法",
                                                                                            "SMTest特性不能用在静态方法之外的地方：'{0}'",
-                                                                                           "XFE各类拓展.NetCore.XUnit.Analyzer",
+                                                                                           "XFE各类拓展.NetCore.XUnit.Analyzer.Diagnostics",
                                                                                            DiagnosticSeverity.Error,
                                                                                            true,
                                                                                            "SMTest特性不能用在静态方法之外的地方.",
                                                                                            "https://www.xfegzs.com/codespace/diagnostics/XFE0001.html");
-        public void Initialize(GeneratorInitializationContext context)
+
+        public override ImmutableArray<DiagnosticDescriptor> SupportedDiagnostics => ImmutableArray.Create(SMTestError);
+
+        public override void Initialize(AnalysisContext context)
         {
+            context.ConfigureGeneratedCodeAnalysis(GeneratedCodeAnalysisFlags.Analyze | GeneratedCodeAnalysisFlags.ReportDiagnostics);
+            context.EnableConcurrentExecution();
+            context.RegisterSyntaxNodeAction(AttributeAnalyzer, SyntaxKind.Attribute);
         }
 
-        public void Execute(GeneratorExecutionContext context)
-        {
-            AttributeAnalyzer(context);
-        }
-        public static void AttributeAnalyzer(GeneratorExecutionContext context)
+        public static void AttributeAnalyzer(SyntaxNodeAnalysisContext context)
         {
             var syntaxTrees = context.Compilation.SyntaxTrees;
             foreach (var syntaxTree in syntaxTrees)
